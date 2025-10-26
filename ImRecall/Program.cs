53685f5b79a5cce7578ae93c3506f3ebcfeb2c71
uwx <maxine@cybergal.com>
2025-10-26 17:50:46 +0100

    myore

--- ImRecall/Program.cs
+++ ImRecall/Program.cs
@@ -1,34 +1,127 @@
 ﻿using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
 using Windows.Graphics.Display;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 using SixLabors.ImageSharp.PixelFormats;
 using GraphicsCaptureItem = Windows.Graphics.Capture.GraphicsCaptureItem;
 
 namespace ImRecall
 {
-    public class Program
+    public partial class Program
     {
+        [GeneratedRegex(
+            """
+            [\0-\u001F"*/:<>?\\\|]
+            """
+        )]
+        private static partial Regex InvalidFileNameCharsRegex { get; }
+        
+        private static string SanitizeFileName(string fileName)
+        {
+            return InvalidFileNameCharsRegex.Replace(fileName, "_");
+        }
+
         public static async Task Main(string[] args)
         {
-            var i = 0;
-            
-            foreach (var (index, displayId) in DisplayServices.FindAll().Index())
+            // TODO remove disconnected displays from here
+            var lastBitmapByDisplayName = new Dictionary<string, Bitmap>();
+
+            try
             {
-                var captureItem = GraphicsCaptureItem.TryCreateFromDisplayId(displayId);
-                if (captureItem == null)
+                while (true)
                 {
-                    Console.WriteLine("WindowsCapture was provided with a invalid item (null) for Windows.Graphics.Capture to capture window... :(");
-                    continue;
+                    foreach (var (index, displayId) in DisplayServices.FindAll().Index())
+                    {
+                        var captureItem = GraphicsCaptureItem.TryCreateFromDisplayId(displayId);
+                        if (captureItem == null)
+                        {
+                            Console.WriteLine($"[{index}] WindowsCapture was provided with a invalid item (null) for Windows.Graphics.Capture to capture window... :(");
+                            continue;
+                        }
+
+                        var bitmap = await CaptureSdr(captureItem);
+                        if (bitmap == null)
+                        {
+                            Console.WriteLine($"[{index}] Failed to capture screenshot.");
+                            continue;
+                        }
+
+                        try
+                        {
+                            if (!lastBitmapByDisplayName.TryGetValue(captureItem.DisplayName, out var lastBitmap) ||
+                                !AreBitmapsSimilar(bitmap, lastBitmap))
+                            {
+                                lastBitmap?.Dispose();
+                                lastBitmapByDisplayName[captureItem.DisplayName] = bitmap;
+
+                                // Save the tonemapped SDR image
+                                var stopwatch = Stopwatch.StartNew();
+                                bitmap.Save($"{SanitizeFileName(captureItem.DisplayName)}-{SanitizeFileName(GetForegroundWindowName())}-{DateTimeOffset.Now:yyyy-MM-dd_HH-mm-ss}.png", ImageFormat.Png);
+                                stopwatch.Stop();
+                                Console.WriteLine($"[{index}] Image saved in {stopwatch.ElapsedMilliseconds} ms.");
+                            }
+                            else
+                            {
+                                bitmap.Dispose();
+                            }
+                        }
+                        catch
+                        {
+                            bitmap.Dispose();
+                            throw;
+                        }
+
+                        continue;
+
+                        static bool AreBitmapsSimilar(Bitmap currentBitmap, Bitmap lastBitmap)
+                        {
+                            using var currentImg = LibraryIndependentImage<Bgr24>.FromBitmap(currentBitmap);
+                            using var lastImg = LibraryIndependentImage<Bgr24>.FromBitmap(lastBitmap);
+                            return SsimUtils.IsSimilar(currentImg, lastImg);
+                        }
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(3));
                 }
-                
+            }
+            finally
+            {
+                foreach (var bitmap in lastBitmapByDisplayName.Values)
+                {
+                    bitmap.Dispose();
+                }
+            }
+        }
+        
+        private static string GetForegroundWindowName()
+        {
+            var hwnd = PInvoke.GetForegroundWindow();
+            if (hwnd != HWND.Null)
+            {
+                Span<char> title = stackalloc char[256];
+                var length = PInvoke.GetWindowText(hwnd, title);
+                if (length > 0)
+                {
+                    return new string(title[..length]);
+                }
+            }
+            return "Desktop";
+        }
+
+        private static async Task<Bitmap?> CaptureSdr(GraphicsCaptureItem captureItem)
+        {
+            Bitmap? bitmap = null;
+            try
+            {
                 // Capture HDR image
                 using var nullableHdrImage = await new WindowsCapture().CaptureFullscreen2(captureItem);
             
                 if (nullableHdrImage is not {} hdrImage)
                 {
-                    Console.WriteLine("Failed to capture HDR image.");
-                    continue;
+                    return null;
                 }
 
                 // Tonemap HDR to SDR
@@ -43,24 +136,19 @@ namespace ImRecall
 
                 var stopwatch = Stopwatch.StartNew();
                 
-                using var bitmap = new Bitmap(hdrImage.Width, hdrImage.Height, PixelFormat.Format24bppRgb);
-                using (var sdrImage = LibraryIndependentImage<Bgr24>.FromBitmap(bitmap))
-                {
-                    HdrTonemapper.TonemapToSdr(hdrImage, sdrImage, tonemapSettings);
-                }
+                bitmap = new Bitmap(hdrImage.Width, hdrImage.Height, PixelFormat.Format24bppRgb);
+                using var sdrImage = LibraryIndependentImage<Bgr24>.FromBitmap(bitmap);
+                HdrTonemapper.TonemapToSdr(hdrImage, sdrImage, tonemapSettings);
 
                 stopwatch.Stop();
                 Console.WriteLine($"Tonemapping completed in {stopwatch.ElapsedMilliseconds} ms.");
-        
-                // Save the tonemapped SDR image
-                stopwatch = Stopwatch.StartNew();
-                bitmap.Save($"{captureItem.DisplayName}-{DateTimeOffset.Now:yyyy-MM-dd_HH-mm-ss}.png", ImageFormat.Png);
-                // await sdrImage.SaveAsWebpAsync($"{captureItem.DisplayName}-{DateTimeOffset.Now:yyyy-MM-dd_HH-mm-ss}.webp", new WebpEncoder
-                // {
-                //     FileFormat = WebpFileFormatType.Lossless,
-                // });
-                stopwatch.Stop();
-                Console.WriteLine($"Image saved in {stopwatch.ElapsedMilliseconds} ms.");
+
+                return bitmap;
+            }
+            catch
+            {
+                bitmap?.Dispose();
+                throw;
             }
         }
     }
--- ImRecall/SimplifiedRowIterator.cs
+++ ImRecall/SimplifiedRowIterator.cs
@@ -41,8 +41,41 @@ public static class SimplifiedRowIterator
             wrappingOperation.Invoke);
     }
     
+    public static void IterateRowsWithIndex<T>(
+        Rectangle rectangle,
+        in T operation,
+        int numOfSteps)
+        where T : struct, IRowOperationWithIndex
+    {
+        int top = rectangle.Top;
+        int bottom = rectangle.Bottom;
+        int width = rectangle.Width;
+        int height = rectangle.Height;
+
+        // Avoid TPL overhead in this trivial case:
+        if (numOfSteps == 1)
+        {
+            for (int y = top; y < bottom; y++)
+            {
+                Unsafe.AsRef(in operation).Invoke(y, 0);
+            }
+
+            return;
+        }
+
+        int verticalStep = DivideCeil(rectangle.Height, numOfSteps);
+        var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = numOfSteps };
+        var wrappingOperation = new RowOperationWithIndexWrapper<T>(top, bottom, verticalStep, in operation);
+
+        Parallel.For(
+            0,
+            numOfSteps,
+            parallelOptions,
+            wrappingOperation.Invoke);
+    }
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static int DivideCeil(long dividend, int divisor) => (int)Math.Min(1 + ((dividend - 1) / divisor), int.MaxValue);
+    internal static int DivideCeil(long dividend, int divisor) => (int)Math.Min(1 + ((dividend - 1) / divisor), int.MaxValue);
 
     [method: MethodImpl(MethodImplOptions.AggressiveInlining)]
     private readonly struct RowOperationWrapper<T>(
@@ -74,4 +107,45 @@ public static class SimplifiedRowIterator
             }
         }
     }
+
+    [method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private readonly struct RowOperationWithIndexWrapper<T>(
+        int minY,
+        int maxY,
+        int stepY,
+        in T action
+    )
+        where T : struct, IRowOperationWithIndex
+    {
+        private readonly T _action = action;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Invoke(int i)
+        {
+            int yMin = minY + (i * stepY);
+
+            if (yMin >= maxY)
+            {
+                return;
+            }
+
+            int yMax = Math.Min(yMin + stepY, maxY);
+
+            for (int y = yMin; y < yMax; y++)
+            {
+                // Skip the safety copy when invoking a potentially impure method on a readonly field
+                Unsafe.AsRef(in _action).Invoke(y, i);
+            }
+        }
+    }
+}
+
+public interface IRowOperationWithIndex
+{
+    /// <summary>
+    /// Invokes the method passing the row y coordinate.
+    /// </summary>
+    /// <param name="y">The row y coordinate.</param>
+    /// <param name="index">The index of the operation.</param>
+    void Invoke(int y, int index);
 }
