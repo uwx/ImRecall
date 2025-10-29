402497aa6ed543fcffb8734232e929ee385e8856
uwx <maxine@cybergal.com>
2025-10-29 01:53:03 +0100

    remove some unused stuff

--- ImRecall/Program.cs
+++ ImRecall/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.Drawing.Imaging;
-using ImRecall;
+﻿using ImRecall;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Org.OpenAPITools.Client;
-using Org.OpenAPITools.Extensions;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
@@ -11,7 +8,6 @@ var builder = Host.CreateDefaultBuilder(args)
         services.AddHostedService<ScreenshotProgram>();
         services.AddTransient<IImmichConfigProvider, ImmichConfigProvider>();
         services.AddTransient<IScreenshotService, ScreenshotService>();
-        services.AddHttpClient<IImmichUploadService, ImmichUploadService>(); // (static (services, client) => client.BaseAddress = new Uri(services.GetRequiredService<ImmichAuth>().Url))
         services.AddTransient<IImmichUploadService, ImmichUploadService>();
         
         services.AddSingleton<ImmichAuth>(static services => services.GetRequiredService<IImmichConfigProvider>().GetAuth());
@@ -19,7 +15,3 @@ var builder = Host.CreateDefaultBuilder(args)
 
 using var host = builder.Build();
 host.Run();
-
-namespace ImRecall
-{
-}
