b10f85f3231cea3c390cfd336e9f9d0ba7611188
uwx <maxine@cybergal.com>
2025-10-29 18:25:57 +0100

    Remove GDI dependency

--- ImRecall/ScreenshotProgram.cs
+++ ImRecall/ScreenshotProgram.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Diagnostics;
 using System.Drawing;
 using System.Text.RegularExpressions;
@@ -5,7 +6,9 @@ using Windows.Graphics.Capture;
 using Windows.Graphics.Display;
 using Windows.Win32;
 using Windows.Win32.Foundation;
+using LibAvifSharp.NativeTypes;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IO;
 using SixLabors.ImageSharp.PixelFormats;
 using WebPWrapper;
 
@@ -22,7 +25,7 @@ public partial class ScreenshotProgram(IScreenshotService screenshotService, IIm
     private static partial Regex InvalidFileNameCharsRegex { get; }
 
     // TODO remove disconnected displays from here
-    private Dictionary<string, Bitmap> lastBitmapByDisplayName = new();
+    private Dictionary<string, LibraryIndependentImage<Bgr24>> lastBitmapByDisplayName = new();
         
     private static string SanitizeFileName(string fileName)
     {
@@ -59,8 +62,8 @@ public partial class ScreenshotProgram(IScreenshotService screenshotService, IIm
                 continue;
             }
 
-            var bitmap = await screenshotService.CaptureScreenshotAsync(captureItem);
-            if (bitmap == null)
+            var bitmapNullable = await screenshotService.CaptureScreenshotAsync(captureItem);
+            if (bitmapNullable is not {} bitmap)
             {
                 Console.WriteLine($"[{index}] Failed to capture screenshot.");
                 continue;
@@ -69,19 +72,45 @@ public partial class ScreenshotProgram(IScreenshotService screenshotService, IIm
             try
             {
                 if (!lastBitmapByDisplayName.TryGetValue(captureItem.DisplayName, out var lastBitmap) ||
-                    !AreBitmapsSimilar(bitmap, lastBitmap))
+                    !SsimUtils.IsSimilar(bitmap, lastBitmap))
                 {
-                    lastBitmap?.Dispose();
+                    if (lastBitmap != default)
+                        lastBitmap.Dispose();
+
                     lastBitmapByDisplayName[captureItem.DisplayName] = bitmap;
 
-                    // Upload to Immich
+                    // Encode to WebP
                     var stopwatch = Stopwatch.StartNew();
+                    #if USE_AVIF
+                    using var bitmapMemoryOwner = Avif.Encode(bitmap, encoder =>
+                    {
+                        encoder.Quality = 100;
+                        encoder.CodecChoice = AvifCodecChoice.AVIF_CODEC_CHOICE_AOM;
+                        // encoder.PixelFormat = AvifPixelFormat.AVIF_PIXEL_FORMAT_YUV420;
+                    });
+                    #elif USE_GDI
+                    using var bitmapMemoryOwner = GDIEncode(bitmap);
+                    #else
                     using var bitmapMemoryOwner = WebP.EncodeLossless(bitmap);
+                    #endif
+                    
+                    stopwatch.Stop();
+                    Console.WriteLine($"[{index}] Encoded to WebP in {stopwatch.ElapsedMilliseconds} ms.");
+                    stopwatch = Stopwatch.StartNew();
+                    
+                    // Upload to Immich
+                    #if DEBUG && DEBUG_DONT_UPLOAD
+                    await using var stream =
+                        File.Create(
+                            $"{SanitizeFileName(captureItem.DisplayName)}-{SanitizeFileName(GetForegroundWindowName())}-{DateTimeOffset.Now:yyyy-MM-dd_HH-mm-ss}.webp");
+                    await stream.WriteAsync(bitmapMemoryOwner.Memory, cancellationToken);
+                    #else
                     await immichUploadService.UploadAsync(
                         $"{SanitizeFileName(captureItem.DisplayName)}-{SanitizeFileName(GetForegroundWindowName())}-{DateTimeOffset.Now:yyyy-MM-dd_HH-mm-ss}.webp",
                         bitmapMemoryOwner.Memory,
                         cancellationToken
                     );
+                    #endif
                     stopwatch.Stop();
                     Console.WriteLine($"[{index}] Image uploaded in {stopwatch.ElapsedMilliseconds} ms.");
                 }
@@ -97,15 +126,38 @@ public partial class ScreenshotProgram(IScreenshotService screenshotService, IIm
             }
 
             continue;
-
-            static bool AreBitmapsSimilar(Bitmap currentBitmap, Bitmap lastBitmap)
-            {
-                using var currentImg = LibraryIndependentImage<Bgr24>.FromBitmap(currentBitmap);
-                using var lastImg = LibraryIndependentImage<Bgr24>.FromBitmap(lastBitmap);
-                return SsimUtils.IsSimilar(currentImg, lastImg);
-            }
         }
     }
+
+#if USE_GDI
+    private readonly RecyclableMemoryStreamManager _manager = new();
+    
+    private unsafe IMemoryOwner<byte> GDIEncode(LibraryIndependentImage<Bgr24> bitmap)
+    {
+        using var ms = _manager.GetStream();
+
+        fixed (byte* ptr = bitmap.MemoryOwner.Memory.Span)
+        {
+            using var bmp = new Bitmap(bitmap.Width, bitmap.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            
+            var bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmp.PixelFormat);
+            try
+            {
+                Buffer.MemoryCopy(ptr, (void*)bmpData.Scan0, bmpData.Width * bmpData.Height * 3, bitmap.Width * bitmap.Height * 3);
+            }
+            finally
+            {
+                bmp.UnlockBits(bmpData);
+            }
+
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+            var buf = new UnmanagedMemoryOwner<byte>((int)ms.Length);
+            ms.GetReadOnlySequence().CopyTo(buf.Memory.Span);
+            return buf;
+        }
+    }
+#endif
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
